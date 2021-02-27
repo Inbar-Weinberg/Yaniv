c2d0d8682d73a188871b9ba5817f0c6cb86d2175
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-27 20:30:36 +0200

    finished 'start game' screen

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import GameDeck from './Classes/Card Classes/GameDeck.js';
 import Pile from './Classes/Card Classes/Pile.js';
 import PlayerHand from './Classes/Card Classes/PlayerHand.js'
-
 import Game from './Classes/Game Classes/Game.js'
-//import {Turn} from './Classes/Game Classes/Game.js'
-let game = new Game();
 
-export {game};
+(function startGame(){
+    const startButton = document.getElementById('start-game-button');
+    startButton.addEventListener(click,()=>{
+
+    })
+})();
 
 
+//  <form id='new-game-form'>*/
 
 
 
